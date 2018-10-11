322bbf610872f10c332be33839de0f0a66d2acb1
Dewangee Agrawal <dewangee@Dewangee.local>
2018-10-11 19:20:28 +0530

    Final Commit fix

--- upload.php
+++ upload.php
@@ -1,10 +1,10 @@
 <?php
     
     
-    if (is_uploaded_file($_FILES['bill']['tmp_name'])) {
+    if (is_uploaded_file($_FILES['myfile']['tmp_name'])) {
         $uploads_dir = './';
-        $tmp_name = $_FILES['bill']['tmp_name'];
-        $pic_name = $_FILES['bill']['name'];
+        $tmp_name = $_FILES['myfile']['tmp_name'];
+        $pic_name = $_FILES['myfile']['name'];
         move_uploaded_file($tmp_name, $uploads_dir.$pic_name);
     }
     else{
